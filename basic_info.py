53b74327bb18b64c3293e12a96deedc148099522
Max White <53018216+maxrgwhite@users.noreply.github.com>
2024-09-09 01:46:08 +0100

    Add files via upload

---
+++ basic_info.py
@@ -0,0 +1,2 @@
+directory_filepath = '/Users/maxwhite/Desktop/Heat_index_Algorithms_to_upload' # Path to the directory where the files are stored (e.g. '/Users/maxwhite/Desktop/Heat_index_Algorithms')
+data_folder = '/Users/maxwhite/Desktop/Heat_index_Algorithms_to_upload/Data'
